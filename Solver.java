566d169ac55752208faff23bb46a77f15c46d445
Eric Fleith <ericfleith@gmail.com>
2019-04-08 13:30:26 -0400

    Update Solver.java

--- Solver.java
+++ Solver.java
@@ -1,6 +1,10 @@
 public class Solver{
-   public static void main(String a[]){
-      Board b = new Board(8,8); 
+   public static void main(String arg[]){
+      Board a = new Board(8,8);
+      a.solver();
+      System.out.println(a.printBoard());
+
+      BoardSimple b = new BoardSimple(8,8);
       b.solver();
       System.out.println(b.printBoard());
    }
